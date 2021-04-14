732ae6c3cdfb723e25a9b6dc8d6594bcb03c0807
Victoria Pisarevsky <1638134+victoria-luidia@users.noreply.github.com>
2021-04-13 18:06:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var rateoutput = document.getElementById("rateoutput").value;
+var years = document.getElementById("years").value;
+var interest = (principal * years * rate) /100;
+var actualyear = (new Date().getFullYear()) + years;
+var spanresult = document.getElementById("result");
+
+function slider(intratevalue){
+    rateoutput = intratevalue;
+    document.getElementById("rateoutput").innerText = rateoutput;
+    
+
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    if (principal <= 0){
+        alert("Enter a positive number!");
+        document.getElementById("principal").focus();
+    }else{
+    rate = document.getElementById("rateoutput").innerText;
+    years = document.getElementById("years").value;
+    interest = (Number(principal) * Number(years) * Number(rate)) /100;
+    yeartobe = (new Date().getFullYear()) + Number(years);
+    computeresult = document.getElementById("result");
+    var text = 'If you deposit $'+principal+',\n'+
+               'at an interest rate of '+rate+'%\n'+
+               'You will receive an amount of $'+interest+',\n'+
+               'in the year '+yeartobe.toString()
+    computeresult.innerText = text
+    }
 }
         
